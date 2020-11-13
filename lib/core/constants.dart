a7fed54fda3388634286935211376a6bb4f4e0c6
Leticia <leticiacardoso31@hotmail.com>
2020-11-13 19:57:32 -0300

    1.1
    tic-tac-toe

---
+++ lib/core/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const int BOARD_SIZE = 9;
+
+const String PLAYER1_SYMBOL = 'Lindinha';
+const Color PLAYER1_COLOR = Colors.lightBlue;
+const String PLAYER1_IMG = 'assets/lindinha.png';
+
+const String PLAYER2_SYMBOL = 'Docinho';
+const Color PLAYER2_COLOR = Colors.green;
+const String PLAYER2_IMG = 'assets/docinho.png';
+
+const String GAME_TITLE = 'Tic Tac Toe';
+const String TIED_TITLE = 'Game Tied!';
+const String WIN_TITLE = 'Player [SYMBOL] won!';
+const String DIALOG_MESSAGE = 'Press the reset button to start again!';
+const String SINGLE_PLAYER_MODE = 'Single Player';
+const String MULTIPLAYER_MODE = 'Two Players';
+const String RESET_BUTTON_LABEL = 'RESET';
